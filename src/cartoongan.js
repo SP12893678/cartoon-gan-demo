f53a9dc918dda6ef7cf314c6eda73cae2311e5c8
SP12893678 <office1289366@gmail.com>
2022-01-04 20:51:03 +0800

    feat: add model

--- src/cartoongan.js
+++ src/cartoongan.js
@@ -7,8 +7,9 @@ export default class CartoonGAN {
     }
 
     async setModel(){   
-        console.log(MyModel)
-        const MODEL_URL = 'https://raw.githubusercontent.com/leemengtaiwan/tfjs-models/master/cartoongan/tfjs_json_models/shinkai/model.json';
+        // console.log(MyModel)
+        const MODEL_URL = 'https://cdn.jsdelivr.net/gh/SP12893678/cartoon-gan-demo@master/src/assets/model/model.json'
+        // const MODEL_URL = 'https://raw.githubusercontent.com/leemengtaiwan/tfjs-models/master/cartoongan/tfjs_json_models/shinkai/model.json';
         let generator = await tf.loadGraphModel(MODEL_URL);
         console.log('generator loaded.');
         console.log('warm up generator');
